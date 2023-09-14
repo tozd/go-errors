68b0da38a1e6b0dfa6630b5a40cf1a739e26a831
Mitar <mitar.git@tnode.com>
2023-09-14 12:34:49 +0200

    Add join example.

---
+++ example_join_test.go
@@ -0,0 +1,33 @@
+package errors_test
+
+import (
+	"fmt"
+	"os"
+
+	"gitlab.com/tozd/go/errors"
+)
+
+func run() (errE errors.E) {
+	file, err := os.CreateTemp("", "test")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer func() {
+		errE = errors.Join(errE, errors.WithStack(os.Remove(file.Name())))
+	}()
+
+	// Do something with the file...
+
+	return nil
+}
+
+func ExampleJoin_defer() {
+	errE := run()
+	if errE != nil {
+		fmt.Printf("error: %+v\n", errE)
+	} else {
+		fmt.Printf("success\n")
+	}
+	// Output:
+	// success
+}
