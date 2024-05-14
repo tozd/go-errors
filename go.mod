06dac2627c8c0b476dc6d15f4d20a95a7cf75e35
Mitar <mitar.git@tnode.com>
2024-05-14 12:42:10 +0200

    Update github.com/stretchr/testify test library.

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	github.com/google/go-cmp v0.5.8
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.9.0
 )
--- stdlib_test.go
+++ stdlib_test.go
@@ -1,8 +1,6 @@
 package errors_test
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,50 +8,35 @@ import (
 	"gitlab.com/tozd/go/errors"
 )
 
-// See: https://github.com/stretchr/testify/issues/1065
-func notImplements(t *testing.T, interfaceObject interface{}, object interface{}, msgAndArgs ...interface{}) bool { //nolint:unparam
-	t.Helper()
-	interfaceType := reflect.TypeOf(interfaceObject).Elem()
-
-	if object == nil {
-		return assert.Fail(t, fmt.Sprintf("Cannot check if nil does not implement %v", interfaceType), msgAndArgs...)
-	}
-	if !reflect.TypeOf(object).Implements(interfaceType) {
-		return true
-	}
-
-	return assert.Fail(t, fmt.Sprintf("%T must not implement %v", object, interfaceType), msgAndArgs...)
-}
-
 func TestBase(t *testing.T) {
 	t.Parallel()
 
 	parent := errors.Base("Foobar")
 	assert.EqualError(t, parent, "Foobar")
 	assert.Nil(t, errors.Unwrap(parent))
-	notImplements(t, (*stackTracer)(nil), parent)
+	assert.NotImplements(t, (*stackTracer)(nil), parent)
 
 	another := errors.Basef("Foobar")
 	assert.EqualError(t, another, "Foobar")
 	assert.Nil(t, errors.Unwrap(another))
-	notImplements(t, (*stackTracer)(nil), another)
+	assert.NotImplements(t, (*stackTracer)(nil), another)
 
 	child := errors.Basef("Foobar (%w)", parent)
 	assert.EqualError(t, child, "Foobar (Foobar)")
 	assert.Equal(t, parent, errors.Unwrap(child))
-	notImplements(t, (*stackTracer)(nil), child)
+	assert.NotImplements(t, (*stackTracer)(nil), child)
 	assert.ErrorIs(t, child, parent)
 
 	child2 := errors.BaseWrap(parent, "Foobar2")
 	assert.EqualError(t, child2, "Foobar2")
 	assert.Equal(t, parent, errors.Unwrap(child2))
-	notImplements(t, (*stackTracer)(nil), child2)
+	assert.NotImplements(t, (*stackTracer)(nil), child2)
 	assert.ErrorIs(t, child2, parent)
 
 	child3 := errors.BaseWrapf(parent, "Foobar3 (%s)", parent)
 	assert.EqualError(t, child3, "Foobar3 (Foobar)")
 	assert.Equal(t, parent, errors.Unwrap(child3))
-	notImplements(t, (*stackTracer)(nil), child3)
+	assert.NotImplements(t, (*stackTracer)(nil), child3)
 	assert.ErrorIs(t, child3, parent)
 
 	// We use w() to prevent static analysis.
